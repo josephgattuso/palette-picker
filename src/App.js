bde0ece18441366b7eec4431eb0b4e016a76fb26
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-02-18 19:06:05 +0000

    chore: update package deps

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    // eslint-disable-next-line no-console
-    console.log(generatePalette(seedColors[0]));
     return (
       <div>
-        <Palette palette={generatePalette(seedColors[0])} />      </div>
-    )
+        <Palette palette={generatePalette(seedColors[0])} />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
